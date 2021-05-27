c4e99d38a1f08cd7027a7f13f7545e1cf99f2a9c
Paolo Catalano <paolo12587@gmail.com>
2021-05-27 20:04:19 +0200

    netlify-identity-widget

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,19 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useContext } from "react";
+import AuthContext from "../stores/authContext";
 
 export default function Navbar() {
+  const user = useContext(AuthContext);
+  console.log(user);
   return (
     <div className="container">
       <nav>
         <Image src="/rupee.png" width={50} height={48} />
         <h1>Gaming Vibes</h1>
-        <h2>Netlify Identity</h2>
+        <p>
+          <h2>Netlify Identity</h2>
+        </p>
         <ul>
           <li>
             <Link href="/">
